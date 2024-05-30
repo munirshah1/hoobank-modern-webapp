13f2da715411145d7c5288e705ccebe24d91aaa4
munirshah1 <munirshahgillani1@gmail.com>
2024-05-30 15:08:10 +0500

    update get started compoenent

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -7,7 +7,7 @@ export default function GetStarted() {
       className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
     >
       <div
-        className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}
+        className={`flex justify-center items-center flex-col bg-primary w-full h-full rounded-full`}
       >
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23.4px]'>
