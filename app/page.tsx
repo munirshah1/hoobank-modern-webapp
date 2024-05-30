754f9eba22cb479a01743cfe2c656ef056272473
munirshah1 <munirshahgillani1@gmail.com>
2024-05-30 15:08:30 +0500

    add state section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from '@/components/HeroSection';
 import Navbar from '@/components/Navbar';
+import StateSection from '@/components/StateSection';
 import styles from '@/styles';
 
 export default function Home() {
@@ -14,6 +15,12 @@ export default function Home() {
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <HeroSection />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <StateSection />
         </div>
       </div>
     </div>
