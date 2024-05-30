ba108cedbd34851f9fa24de5674dec615cac4726
munirshah1 <munirshahgillani1@gmail.com>
2024-05-30 15:19:52 +0500

    add business section compoenent

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BusinessSection from '@/components/BusinessSection';
 import HeroSection from '@/components/HeroSection';
 import Navbar from '@/components/Navbar';
 import StateSection from '@/components/StateSection';
@@ -21,6 +22,7 @@ export default function Home() {
       <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <StateSection />
+          <BusinessSection />
         </div>
       </div>
     </div>
